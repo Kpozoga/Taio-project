8b5dede7368804d1863d5d99a3a2f4f01abd8070
Kpozoga <44911279+Kpozoga@users.noreply.github.com>
2021-11-10 19:35:58 +0100

    init

---
+++ Taio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Taio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
