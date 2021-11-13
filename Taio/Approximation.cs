eb6d5c95204d064b1c610ff5b88b302be380d66e
BridgeFan <kborycz@gmail.com>
2021-11-13 12:09:30 +0100

    Begun implementation
    Created the most basic function and gitignore.

---
+++ Taio/Approximation.cs
@@ -0,0 +1,12 @@
+namespace Taio
+{
+    public class Approximation
+    {
+        static (int distance, int[] nodes) ApproximateDistance(bool[,] graph1, bool[,] graph2)
+        {
+            
+            
+            return (0, new [] {0});
+        }
+    }
+}
