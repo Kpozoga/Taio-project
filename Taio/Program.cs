87073190c6e853c2880454e18de5bd79f8eef311
KP <kpozoga99@gmail.com>
2021-11-13 23:20:30 +0100

    reading graphs + calculating subgraph

--- Taio/Program.cs
+++ Taio/Program.cs
@@ -19,7 +19,41 @@ namespace Taio
         }
         public static int GetDistance(bool[,] graph1, bool[,] graph2, int[] equivalence)
         {
-            return CountEdges(graph1) + CountEdges(graph2) - Math.Abs(graph1.GetLength(0) - graph2.GetLength(0));
+            return CountEdges(graph1) + CountEdges(graph2) - 2 * CountEdges(GetSubgraph(graph1, graph2)) + Math.Abs(graph1.GetLength(0) - graph2.GetLength(0));
+        }
+        public static bool[,] ReadGraph(int n)
+        {
+            var graph = new bool[n,n];
+            for(int i=0;i<n;i++)
+            {
+                var line = Console.ReadLine();
+                var splitted = line.Split(' ');
+                for(int j=0;j<n;j++)
+                {
+                    int.TryParse(splitted[j], out int num);                   
+                    graph[i, j] = Convert.ToBoolean(num);
+                }
+            }
+            return graph;
+        }
+        public static bool[,] GetSubgraph(bool[,] g1, bool[,] g2)
+        {
+            int nMin, nMax;
+            if(g1.GetLength(0) > g2.GetLength(0))
+            {
+                nMax = g1.GetLength(0);
+                nMin = g2.GetLength(0);
+            }
+            else
+            {
+                nMax = g2.GetLength(0);
+                nMin = g1.GetLength(0);
+            }
+            var subgraph = new bool[nMax, nMax];
+            for(int i=0;i<nMin;i++)               
+                for(int j=0;j<nMin;j++)
+                    subgraph[i,j] = g1[i,j] && g2[i,j];
+            return subgraph;
         }
     }
     class Program
@@ -27,10 +61,12 @@ namespace Taio
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            var graph1 = new bool[3, 3];
-            graph1[1, 0] = true;
-            graph1[1, 2] = true;
+            int.TryParse(Console.ReadLine(), out int n1);
+            var graph1 = Util.ReadGraph(n1);
+            int.TryParse(Console.ReadLine(), out int n2);
+            var graph2 = Util.ReadGraph(n2);
             var result = Approximation.GetDistance(graph1, graph1);
+            Console.WriteLine(result);
         }
     }
 }
