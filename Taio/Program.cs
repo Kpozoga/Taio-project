da48310004bb81417818181e76f9c621475565b4
ztyszka <zu.tyszka@gmail.com>
2021-11-15 00:05:27 +0100

    próba implementacji dokładnego algorytmu

--- Taio/Program.cs
+++ Taio/Program.cs
@@ -7,12 +7,15 @@ namespace Taio
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please, enter data: ");
             int.TryParse(Console.ReadLine(), out int n1);
             var graph1 = Util.ReadGraph(n1);
             int.TryParse(Console.ReadLine(), out int n2);
             var graph2 = Util.ReadGraph(n2);
             var result = Approximation.GetDistance(graph1, graph2);
-            Console.WriteLine("Approximation: {0}", result);      
+            Console.WriteLine("Approximation: {0}", result.distance);
+            int exactDist = ExactAlgorithm.GetExactDistance(graph1, graph2);
+            Console.WriteLine("Exact algorithm: {0}", exactDist);
         }
     }
 }
--- Taio/exactAlgorithm.cs
+++ Taio/exactAlgorithm.cs
@@ -1,10 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Taio.Utils;
 
 namespace Taio
 {
-    public static class exactAlgorithm
+    public static class ExactAlgorithm
     {
+        public static int GetExactDistance(bool[,] graph1, bool[,] graph2)
+        {
+            int dist = 0;
+            int n = Math.Min(graph1.GetLength(0), graph2.GetLength(0));
+            int m = Math.Max(graph1.GetLength(0), graph2.GetLength(0));
+            bool[,] graphBase = new bool[n, n];
+            bool[,] graphPermuted = new bool[m, m];
+            if (graph1.GetLength(0) > graph2.GetLength(0))
+            {
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < n; j++)
+                        graphBase[i, j] = graph2[i, j];
+                for (int i = 0; i < m; i++)
+                    for (int j = 0; j < m; j++)
+                        graphPermuted[i, j] = graph1[i, j];
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < n; j++)
+                        graphBase[i, j] = graph1[i, j];
+                for (int i = 0; i < m; i++)
+                    for (int j = 0; j < m; j++)
+                        graphPermuted[i, j] = graph2[i, j];
+            }
+            Permute(m, graphBase, ref graphPermuted, ref dist);
+            return dist;
+        }
+
+        public static void Permute(int k, bool[,] graphBase, ref bool[,] graphPermuted, ref int dist)
+        {
+            if (k == 1)
+            {
+                int tmpDist = Util.GetDistance(graphBase, graphPermuted);
+                if (tmpDist < dist)
+                    dist = tmpDist;
+            }
+            else
+            {
+                for (int i=0; i < k ; i++)
+                {
+                    Swap(ref graphPermuted, i, k - 1);
+                    Permute(k - 1, graphBase, ref graphPermuted, ref dist);
+                    Swap(ref graphPermuted, i, k - 1);
+                }
+            }
+        }
+
+        public static void Swap(ref bool[,] graph, int i, int j)
+        {
+            int n = graph.GetLength(0);
+            bool[,] graphCopy = new bool[n, n];
+            for (int a = 0; a < n; a++)
+                for (int b = 0; b < n; b++)
+                    graphCopy[a, b] = graph[a, b];
+
+            for (int k = 0; k < n; k++)
+            {
+                graph[i, k] = graphCopy[k, j];
+                graph[k, i] = graphCopy[j, k];
+            }
+        }
+
     }
 }
