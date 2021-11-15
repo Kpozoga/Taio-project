0f0ffe8431e537a46774ca59edf163eae9752e00
BridgeFan <kborycz@gmail.com>
2021-11-15 17:28:34 +0100

    Added showing graph and minor improvements
    Added showing graph based on equivalence array.

--- Taio/Program.cs
+++ Taio/Program.cs
@@ -7,13 +7,38 @@ namespace Taio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please, enter data: ");
-            int.TryParse(Console.ReadLine(), out int n1);
-            var graph1 = Util.ReadGraph(n1);
-            int.TryParse(Console.ReadLine(), out int n2);
-            var graph2 = Util.ReadGraph(n2);
+            bool[,] graph1=null, graph2=null;
+            bool pr = true;
+            if (args!=null && args.Length > 0)
+            {
+                pr = false;
+                try
+                {
+                    (graph1, graph2) = Util.ReadFromFile(args[0]);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception in reading file "+args[0]+". Get data from console");
+                    pr = true;
+                }
+                if(!pr)
+                    Console.WriteLine(args[0]+"File loaded successfully");
+            }
+            else
+            {
+                Console.WriteLine("No file provided in args. Get data from console.");
+            }
+            if(pr)
+            {
+                Console.WriteLine("Please, enter data: ");
+                int.TryParse(Console.ReadLine(), out int n1);
+                graph1 = Util.ReadGraph(n1);
+                int.TryParse(Console.ReadLine(), out int n2);
+                graph2 = Util.ReadGraph(n2);
+            }
             var result = Approximation.GetDistance(graph1, graph2);
             Console.WriteLine("Approximation: {0}", result.distance);
+            Util.WriteGraph(graph1, graph2, result.nodes);
             int exactDist = ExactAlgorithm.GetExactDistance(graph1, graph2);
             Console.WriteLine("Exact algorithm: {0}", exactDist);
         }
--- Taio/Utils/Util.cs
+++ Taio/Utils/Util.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Taio.Utils
 {
@@ -19,11 +20,11 @@ namespace Taio.Utils
         }
         public static int GetDistance(bool[,] graph1, bool[,] graph2, int[] equivalence)
         {
-            return CountEdges(graph1) + CountEdges(graph2) - 2 * CountEdges(GetSubgraph(graph1, graph2, equivalence)) - Math.Abs(graph1.GetLength(0) - graph2.GetLength(0));
+            return CountEdges(graph1) + CountEdges(graph2) - 2 * CountEdges(GetSubgraph(graph1, graph2, equivalence)) + Math.Abs(graph1.GetLength(0) - graph2.GetLength(0));
         }
         public static int GetDistance(bool[,] graph1, bool[,] graph2)
         {
-            return CountEdges(graph1) + CountEdges(graph2) - 2 * CountEdges(GetSubgraph(graph1, graph2)) - Math.Abs(graph1.GetLength(0) - graph2.GetLength(0));
+            return CountEdges(graph1) + CountEdges(graph2) - 2 * CountEdges(GetSubgraph(graph1, graph2)) + Math.Abs(graph1.GetLength(0) - graph2.GetLength(0));
         }
         public static bool[,] GetSubgraph(bool[,] g1, bool[,] g2, int[] eq)
         {
@@ -78,6 +79,127 @@ namespace Taio.Utils
                 }
             }
             return graph;
+        }
+
+        public static (bool[,] graph1, bool[,] graph2) ReadFromFile(string path)
+        {
+            string[] lines = System.IO.File.ReadAllLines(path);
+            int n1 = int.Parse(lines[0]);
+            var graph1 = new bool[n1, n1];
+            for (int i = 0; i < n1; i++)
+            {
+                var line = lines[i+1];
+                var splitted = line.Split(' ');
+                for (int j = 0; j < n1; j++)
+                {
+                    int.TryParse(splitted[j], out int num);
+                    graph1[i, j] = Convert.ToBoolean(num);
+                }
+            }
+            int n2 = int.Parse(lines[n1 + 1]);
+            var graph2 = new bool[n1, n1];
+            for (int i = 0; i < n2; i++)
+            {
+                var line = lines[n1+i+2];
+                var splitted = line.Split(' ');
+                for (int j = 0; j < n2; j++)
+                {
+                    int.TryParse(splitted[j], out int num);
+                    graph2[i, j] = Convert.ToBoolean(num);
+                }
+            }
+            return (graph1, graph2);
+        }
+
+        static public void WriteGraph(bool[,] graph1, bool[,] graph2, int[] equivalence)
+        {
+            const int consoleWidth = 40;
+            int n1 = graph1.GetLength(0);
+            int n2 = graph2.GetLength(0);
+            if (Math.Max(n1, n2) > consoleWidth) //not writing graph because it is too large
+                return;
+            (int i1, int i2)[] table = new (int,int)[Math.Max(n1,n2)];
+            bool[] tmpGraph2 = new bool[n2];
+            int p = 0, k = equivalence.Length - 1;
+            for (int i = 0; i < equivalence.Length; i++)
+            {
+                if (equivalence[i] != -1)
+                {
+                    table[p] = (i, equivalence[i]);
+                    tmpGraph2[equivalence[i]] = true;
+                    p++;
+                }
+                else
+                {
+                    table[k] = (i, -1);
+                    k--;
+                }
+            }
+
+            if (n2 > n1)
+            {
+                p = n1;
+                for (int i = 0; i < tmpGraph2.Length; i++)
+                {
+                    if (!tmpGraph2[i])
+                    {
+                        table[p] = (-1, i);
+                        p++;
+                    }
+                }
+            }
+            if (n1 + n2 + 1 < consoleWidth)
+            {
+                //write two graphs next to each other
+                Console.Write(n1);
+                for (int i = (int)Math.Ceiling(Math.Log10(n1 + 1)); i < n1 + 1; i++)
+                {
+                    Console.Write(' ');
+                }
+                Console.WriteLine(n2);
+                for (int i = 0; i < Math.Min(n1, n2); i++)
+                {
+                    for(int j=0;j<n1;j++)
+                        Console.Write(graph1[table[i].i1, table[j].i1] ? 1 : 0);
+                    Console.Write(" ");
+                    for(int j=0;j<n2;j++)
+                        Console.Write(graph2[table[i].i2, table[j].i2] ? 1 : 0);
+                    Console.WriteLine();
+                }
+                for (int i = Math.Min(n1,n2); i < n1; i++)
+                {
+                    for(int j=0;j<n1;j++)
+                        Console.Write(graph1[table[i].i1, table[j].i1] ? 1 : 0);
+                    Console.WriteLine();
+                }
+                for (int i = Math.Min(n1,n2); i < n2; i++)
+                {
+                    for(int j=0;j<=n1;j++)
+                        Console.Write(" ");
+                    for(int j=0;j<n2;j++)
+                        Console.Write(graph2[table[i].i2, table[j].i2] ? 1 : 0);
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                //write second graph after first one
+                Console.WriteLine(n1);
+                for (int i = 0; i < n1; i++)
+                {
+                    for (int j = 0; j < n1; j++)
+                        Console.Write(graph1[table[i].i1, table[j].i1] ? 1 : 0);
+                    Console.WriteLine();
+                }
+                Console.WriteLine(n2);
+                for (int i = 0; i < n2; i++)
+                {
+                    for (int j = 0; j < n2; j++)
+                        Console.Write(graph2[table[i].i2, table[j].i2] ? 1 : 0);
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
